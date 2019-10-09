a843ca62139b5c4c485726bf5a30cd7194afd6ef
Aluno Mack <alunomack@10-44-48-136.0200.mack.br.iosda.org>
2019-10-09 10:34:39 -0300

    Initial Project

---
+++ ProjetoMusica/ProjetoMusica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetoMusica
+//
+//  Created by Aluno Mack on 09/10/19.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
