838afa926171903e4da75f6ffb525b13c2137bb6
Aluno Mack <alunomack@10-44-49-62.0200.mack.br.iosda.org>
2019-10-21 08:33:36 -0300

    Criamos as classes das telas

---
+++ ProjetoMusica/ProjetoMusica/Seleciona_nivel_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Seleciona_nivel_ViewController.swift
+//  ProjetoMusica
+//
+//  Created by Aluno Mack on 21/10/19.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class Seleciona_nivel_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProjetoMusica/ProjetoMusica/Tela_Pergunta_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Tela_Pergunta_ViewController.swift
+//  ProjetoMusica
+//
+//  Created by Aluno Mack on 21/10/19.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class Tela_Pergunta_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
